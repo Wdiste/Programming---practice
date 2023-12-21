dedfd9ef9be51bdda0ad345b2c48986da5f8c802
wdiste <Wdiste49@gmail.com>
2023-12-21 10:53:58 -0500

    initialized allStar

---
+++ allStarChallenge23/allStar.js
@@ -0,0 +1,14 @@
+function scoring(array){
+    
+    
+    };
+
+// var player1 = {
+//   name: "JuanPablo",
+//   normKill: 5,
+//   assist: 12,
+//   damage: 3200,
+//   healing: 0,
+//   streak: 4,
+//   envKill: 1
+// }
